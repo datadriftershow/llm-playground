a88d89f87017eee4884c5eb41555cfb3ccb74985
datadrifter <datadriftershow@gmail.com>
2025-08-07 16:55:54 -0700

    move models out of repo

--- quantize/run.sh
+++ quantize/run.sh
@@ -2,7 +2,7 @@ podman run --rm \
   --device nvidia.com/gpu=all \
   --security-opt=label=disable \
   -v ./quantized-models:/app/quantized_model \
-  -v /home/mentat/workspace/llm-hello-world/models:/app/models \
+  -v /home/mentat/workspace/models:/app/models \
   -e MODEL_PATH="/app/models/Skywork-o1-Open-Llama-3.1-8B" \
   -e QUANT_METHOD="awq" \
   -e BITS="4" \
--- vllm-flash-slm/run.sh
+++ vllm-flash-slm/run.sh
@@ -5,5 +5,5 @@ podman run \
   --security-opt=label=disable \
   --shm-size=10g \
   -p 8000:8000 \
-  -v /home/mentat/workspace/llm-hello-world/models:/app/models \
+  -v /home/mentat/workspace/models:/app/models \
   vllm-flash
